b6e34b2810f423ee9cd02eabddc04f88c2bbafac
Sounagudlowd1 <eoslan2@uic.edu>
2017-12-06 17:32:18 -0600

    Added outputs from each method and files

--- src/data_preprocessing/learning_helpers.py
+++ src/data_preprocessing/learning_helpers.py
@@ -23,12 +23,14 @@ def greedy_feature_selection(learner, X_train, y_train, X_test, y_test,tolerance
         if(debug == True):
             print(best_feature)
             print(max_model_accuracy)
-        selected_features.append(best_feature)
-        unselected_features.remove(best_feature)
-        if(model_accuracy - accuracy < tolerance):
+            
+        if(model_accuracy - accuracy < tolerance or unselected_features == []):
             break
         else:
             accuracy = model_accuracy
+        selected_features.append(best_feature)
+        unselected_features.remove(best_feature)
+
     learner.fit(X_train[selected_features], y_train)
     y_pred = learner.predict(X_test[selected_features])
     return learner, selected_features, r2_score(y_test, y_pred)
