302b34f0880a90e4e24721837e9f7b4339d9eb74
Latika Bhurani <latika1912@gmail.com>
2017-12-06 01:09:51 -0600

    Commit for handling missing data

--- src/data_preprocessing/normalize.py
+++ src/data_preprocessing/normalize.py
@@ -45,8 +45,8 @@ def remove_correlated_features(iData):
     
     iData = iData.drop('TotRmsAbvGrd', 1)
     
-    #highly correlated with Garage area
-    iData = iData.drop('GarageCars', 1)
+    #highly correlated with Garage Cars
+    iData = iData.drop('GarageArea', 1)
     
     #highly correlated with year built
     iData = iData.drop('GarageYrBlt', 1)
@@ -55,7 +55,15 @@ def remove_correlated_features(iData):
     
     return iData
 
-
+# Function to impute missing values
+def feat_impute(iData, columns):
+    for column in columns:
+        if iData[column].dtype ==np.object:
+            iData.loc[iData[column].isnull(),column] = 'None'
+        else:
+            iData.loc[iData[column].isnull(),column] = 0
+            
+    return iData
 
 def fill_in_missing_values(iData):
     values = {}
@@ -98,5 +106,44 @@ def drop_cols_with_missing_data(iData):
     total = iData.isnull().sum().sort_values(ascending=False)
     percent = (iData.isnull().sum()/iData.isnull().count()).sort_values(ascending=False)
     missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-    iData = iData.drop(iData.loc[iData['Electrical'].isnull()].index)
-    return iData.drop((missing_data[missing_data['Total'] >= 8]).index,1)
+    return iData.drop((missing_data[missing_data['Percent'] >= 0.50]).index,1)
+
+def handle_missing_data(iData):
+    
+    # columns with more than 50% missing data
+    iData = drop_cols_with_missing_data(iData)
+    
+    print(iData.columns)
+    #impute features with appropriate values
+    
+    # fireplace
+    iData['FireplaceQu'] = iData['FireplaceQu'].fillna('None')
+    
+    # lot frontage
+    iData['LotFrontage'] = iData['LotFrontage'].fillna(iData['LotFrontage'].median())
+    
+    # garageX variable
+    iData = feat_impute(iData, ['GarageYrBlt','GarageType','GarageQual','GarageCond','GarageFinish'])
+    iData = feat_impute(iData, ['GarageCars','GarageArea'])
+    
+    # Basement Data
+    iData = feat_impute(iData, ['BsmtFinSF1','BsmtUnfSF','BsmtFinSF2'])
+    iData = feat_impute(iData, ['BsmtFinType2','BsmtFinType1', 'BsmtExposure', 'BsmtCond','BsmtQual' ])
+    iData = feat_impute(iData, ['BsmtHalfBath','BsmtFullBath'])
+    iData = feat_impute(iData, ['TotalBsmtSF'])
+    
+    # can be done using criteria on BsmtFinType2 = 'Unf'
+    iData.iloc[1420, iData.columns.get_loc('BsmtExposure')] = 'No'
+    iData.iloc[1940, iData.columns.get_loc('BsmtExposure')] = 'No'
+    
+    # electrical - only one value missing - impute with mode
+    iData['Electrical'] = iData['Electrical'].fillna(iData['Electrical'].mode()[0])
+    
+    # impute masonry data
+    iData['MasVnrArea'] = iData['MasVnrArea'].fillna(iData['MasVnrArea'].mode()[0])
+    iData['MasVnrType'] = iData['MasVnrType'].fillna(iData['MasVnrType'].mode()[0])
+    
+    print(iData.shape)
+    return iData
+    
+    
